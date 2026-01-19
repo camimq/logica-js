c2e1b8ec715c083e9cafc5776ab1bdba15b9002c
Camila <camimq85@gmail.com>
2026-01-19 18:22:16 -0300

    inclusão de novo código para Aula 2: Condicionais e concatenação.

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 5;
+let numeroSecreto = 4;
+console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10.');
 
+console.log(chute == numeroSecreto)
+// se chute for igual ao número secreto; executa.
 if(chute == numeroSecreto){
-  console.log('Isso aí! Você descobriu o número secreto(5)!');
+  alert(`Isso aí! Você descobriu o n[umero secreto ${numeroSecreto}!`)
+} else{
+  alert('Você errou! :(')
 }
