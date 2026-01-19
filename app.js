c281f2becd6d274a09df70999aa8fd1a1f9e6b1c
Camila <camimq85@gmail.com>
2026-01-19 19:35:22 -0300

    Adiciona exercício de prática da Aula 3: Loops e tentativas.

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 4;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10.');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-console.log(chute == numeroSecreto)
-// se chute for igual ao número secreto; executa.
-if(chute == numeroSecreto){
-  alert(`Isso aí! Você descobriu o n[umero secreto ${numeroSecreto}!`)
-} else{
-  alert('Você errou! :(')
+// enquanto chute não for igual ao número secreto
+while(chute != numeroSecreto){
+  chute = prompt('Escolha um número entre 1 e 10.');
+  // se chute for igual ao número secreto; executa.
+  if(chute == numeroSecreto){
+    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}, com ${tentativas} tentativas!`);
+  } else{
+    if(chute > numeroSecreto){
+      alert(`O número é menor que que ${chute}`);
+    }
+    else{
+      alert(`O número é maior que que ${chute}`);
+    }
+    // tentativas = tentativas + 1
+    tentativas++;
+  }
 }
