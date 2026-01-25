612856eee11a28f74af34ee2dab5042d51ab370d
Camila <camimq85@gmail.com>
2026-01-25 14:41:19 -0300

    Adiciona resolução de exercícios da Aula 3 - Reiniciando o jogo.

--- js-curso-2-aula1/js-curso-2-desafio_1/app.js
+++ js-curso-2-aula1/js-curso-2-desafio_1/app.js
@@ -50,7 +50,7 @@ function dobraNumero(num){
 
 // 9. Criar uma função que recebe três números como parâmetros e retorna a média deles.
 function calculaMedia(num1, num2, num3){
-  media = (num1 + num2 + num3) / 3;
+  let media = (num1 + num2 + num3) / 3;
   return console.log(media);
 }
 
@@ -84,3 +84,65 @@ function calculoMultiplicacao(numero){
 }
 
 calculoMultiplicacao(2);
+
+// 12. Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+function indiceIMC(altura, peso){
+  let imc;
+  return console.log(imc = peso / (altura * altura))
+}
+
+indiceIMC(175,100)
+
+// 13. Crie uma função que calcule o valor do fatorial de um número passado como parâmetro.
+// O cálculo fatorial consiste no resultado da multiplicação de um número, por todos os seus antecessores até 1.
+function calculoFatorial(numero){
+  if (numero === 0 || numero === 1){
+    return 1;
+  }
+
+  let fatorial = 1;
+  for (let i = 2; i <= numero; i++){
+    fatorial *= i;
+  }
+  return fatorial
+}
+console.log(calculoFatorial(5))
+
+// 14. Crie uma função que converte um valor em dólar, passado como parâmetro, e retorna o valor equivalente em reais. Para isso, considere a cotação do dólar igual a R$4,80.
+function converteDolar(valorDolar){
+  let conversao = valorDolar * 4.80
+  // toFixed(), é usado para garantir duas casas decimais.
+  return console.log(`$ ${valorDolar.toFixed(2)} equivale a R$ ${conversao.toFixed(2)}, na cotação de hoje (R$ 4.80).`)
+}
+converteDolar(1.00)
+
+// 15. Crie uma função que mostre na tela a área e o perímetro de uma sala retangular, utilizando altura e largura que serão dadas como parâmetro.
+function calcularRetangulo (altura, largura){
+  let area = altura * largura;
+  let perimetro = 2 * (altura + largura)
+
+  return console.log(`A área do retângulo é ${area}; enquanto o perímetro é ${perimetro}.`)
+}
+calcularRetangulo(1,1)
+
+
+// 16. Crie uma função que mostre na tela a área e o perímetro de uma sala circular, utilizando seu raio que será fornecido como parâmetro. Considere Pi = 3,14.
+function calcularCirculo(raio){
+  let pi = 3.14;
+  let area = pi * (raio ** 2);
+  let perimetro = 2 * pi * raio;
+
+  return console.log(`A área do círculo é ${area.toFixed(2)}; enquanto o perímetro é ${perimetro.toFixed(2)}.`);
+}
+calcularCirculo(1)
+
+// 17. Crie uma função que mostre na tela a tabuada de um número dado como parâmetro.
+function mostraTabuada(numero){
+  for (let i = 1; i <= 10; i++){
+    let resultado = numero * i;
+    
+    console.log(`${numero} x ${i} = ${resultado}`)
+  }
+}
+
+mostraTabuada(5)
