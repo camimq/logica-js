696daf5f89a7e10addf097e872d1446bca404995
Camila <camimq85@gmail.com>
2026-01-25 15:16:20 -0300

    feat: adiciona exercícios da Aula 4 - Listas

--- js-curso-2-aula1/js-curso-2-desafio_1/app.js
+++ js-curso-2-aula1/js-curso-2-desafio_1/app.js
@@ -146,3 +146,25 @@ function mostraTabuada(numero){
 }
 
 mostraTabuada(5)
+
+// 18. Crie uma lista vazia, com o nome listaGenerica.
+let listaGenerica = [];
+console.log(listaGenerica);
+
+// 19. Crie uma lista de linguagens de programação chamada linguagensDeProgramacao com os seguintes elementos: 'JavaScript','C','C++', 'Kotlin' e 'Python'.
+let linguagensDeProgramacao = ['JavaScript','C','C++', 'Kotlin', 'Python'];
+
+// 20. Adicione à lista linguagensDeProgramacao os seguintes elementos: 'Java', 'Ruby' e 'GoLang'.
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang')
+console.log(linguagensDeProgramacao);
+
+
+// 21. Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+let listaNomes = ['João', 'Maria', 'José'];
+console.log(listaNomes[0])
+
+// 22. Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+console.log(listaNomes[1])
+
+// 23. Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+console.log(listaNomes[2])
